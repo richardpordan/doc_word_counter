a0eacfe21a68f50ceeeb664359517277082d736c
Richard Pordan <105643799+richardpordan@users.noreply.github.com>
2025-05-17 11:08:44 +0100

    update mockrun

--- test/mock_run.py
+++ test/mock_run.py
@@ -1,34 +1,48 @@
 """WIP playground"""
 
+
 import sys
 import importlib.util
 
 
+# Import main module
 spec = importlib.util.spec_from_file_location("wordcounter", "wordcounter.py")
 main_module = importlib.util.module_from_spec(spec)
 sys.modules["wordcounter"] = main_module
 spec.loader.exec_module(main_module)
-
 wcounter = main_module.WordCounter()
 
+
+# Custom context manager for cli func
+class SysArgvContext:
+    def __init__(self, args: list):
+        self.args = args
+        self.sys_argv_backup = sys.argv
+
+    def __enter__(self):
+        sys.argv = [self.sys_argv_backup[0]] + self.args
+        return self.args
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        sys.argv = self.sys_argv_backup
+
+
+# Set up constants
 CORRECT_COUNT = 16
 TEST_FILES = ["./test/data/test_file.ipynb", "./test/data/test_file.md"]
 TEST_TARGETS = [10, 20]
-OLD_SYS_ARGV = sys.argv
 
+
+# Mock run
 for file_path in TEST_FILES:
     assert(wcounter.count_words(file_path) == 16)
     print("count_words tested on", file_path)
 
-    args = [file_path]
-    sys.argv = [OLD_SYS_ARGV[0]] + args
-    wcounter.cli_run()
-    print("cli_run tested on", args)
-    sys.argv = OLD_SYS_ARGV
+    with SysArgvContext([file_path]) as custom_args:
+        wcounter.cli_run()
+        print("cli_run tested on", custom_args)
     
     for target in TEST_TARGETS:
-        args = [file_path, target]
-        sys.argv = [OLD_SYS_ARGV[0]] + args
-        wcounter.cli_run()
-        print("cli_run tested on", args)
-        sys.argv = OLD_SYS_ARGV
+        with SysArgvContext([file_path, target]) as custom_args:
+            wcounter.cli_run()
+            print("cli_run tested on", custom_args)
